5286a777a5883ba5943c13e17853a6b231ae00bb
Blake Holifield <bholifie@redhat.com>
2024-03-25 14:47:02 -0400

    add debug logging for s3

--- src/common/objectStore.ts
+++ src/common/objectStore.ts
@@ -4,12 +4,14 @@ import config from './config';
 
 export const MinioClient = () => {
   if (config?.objectStore.tls) {
+    apiLogger.debug('s3 config');
     return new Minio.Client({
       endPoint: config?.objectStore.buckets[0].endpoint,
       accessKey: config?.objectStore.buckets[0].accessKey,
       secretKey: config?.objectStore.buckets[0].secretKey,
     });
   }
+  apiLogger.debug('minio config');
   return new Minio.Client({
     endPoint: config?.objectStore.buckets[0].endpoint,
     port: config?.objectStore.port,
@@ -23,9 +25,11 @@ export const uploadPDF = async (id: string, path: string) => {
   const bucket = config?.objectStore.buckets[0].name;
   apiLogger.debug(`${JSON.stringify(config?.objectStore)}`);
   const mc = MinioClient();
+  apiLogger.debug(`${JSON.stringify(mc)}`);
   try {
     const exists = await mc.bucketExists(bucket);
     if (!exists) {
+      apiLogger.debug('Creating a new bucket');
       await mc.makeBucket(bucket, 'us-east-1');
     }
     const metadata = {
@@ -41,6 +45,7 @@ export const uploadPDF = async (id: string, path: string) => {
 export const downloadPDF = async (id: string) => {
   const bucket = config?.objectStore.buckets[0].name;
   const mc = MinioClient();
+  apiLogger.debug(`${JSON.stringify(mc)}`);
   try {
     const stream = await mc.getObject(bucket, `${id}.pdf`);
     apiLogger.debug(`PDF found downloading as ${id}.pdf`);
